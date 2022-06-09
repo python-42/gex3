895cde450b97c1d9e669ecce8798222c33379e5d
Jake <herrmann.jake@protonmail.com>
2022-06-09 18:08:40 -0500

    Call initNavblockConstants on server start

--- src/main/java/com/giftexchange/gex3/Gex3Application.java
+++ src/main/java/com/giftexchange/gex3/Gex3Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Gex3Application {
 
 	public static void main(String[] args) {
+		Constants.NavblockConstants.initNavblockConstants();
 		SpringApplication.run(Gex3Application.class, args);
 	}
 }

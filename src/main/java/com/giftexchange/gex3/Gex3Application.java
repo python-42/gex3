ee103658bc593eb4be309625fb9307b39a7bd3fb
Jake <herrmann.jake@protonmail.com>
2022-06-09 17:57:23 -0500

    Initial commit

---
+++ src/main/java/com/giftexchange/gex3/Gex3Application.java
@@ -0,0 +1,12 @@
+package com.giftexchange.gex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gex3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gex3Application.class, args);
+	}
+}

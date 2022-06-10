b6d9efca5d8e2bb44a3d958490b18a7647bf1459
Jake <herrmann.jake@protonmail.com>
2022-06-10 17:21:14 -0500

    Got rid of index because it wasnt necessary Login is now required to access root

--- src/main/java/com/giftexchange/gex3/WebSecurityConfig.java
+++ src/main/java/com/giftexchange/gex3/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig {
      public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests()
-            .antMatchers("/", "/create", "/css/*", "/favicon.ico").permitAll()//Remember to allow access to static resources such as stylesheets
+            .antMatchers("/create", "/css/*", "/favicon.ico").permitAll()//Remember to allow access to static resources such as stylesheets
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
--- src/main/java/com/giftexchange/gex3/controllers/ManageController.java
+++ src/main/java/com/giftexchange/gex3/controllers/ManageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ManageController {
     
-    @GetMapping("/manage")
+    @GetMapping("/")
     public String content(){
         return "manage";
     }

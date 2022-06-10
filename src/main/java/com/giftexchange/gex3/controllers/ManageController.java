0e67d604970ce910830e3177e91f819a7ca313f3
Jake <herrmann.jake@protonmail.com>
2022-06-10 17:18:21 -0500

    Added manage placeholder

---
+++ src/main/java/com/giftexchange/gex3/controllers/ManageController.java
@@ -0,0 +1,13 @@
+package com.giftexchange.gex3.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ManageController {
+    
+    @GetMapping("/manage")
+    public String content(){
+        return "manage";
+    }
+}

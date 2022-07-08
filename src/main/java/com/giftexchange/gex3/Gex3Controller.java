b3b976052c23b0b8f76f66de1cc49ab6d5e22dcd
Jake <herrmann.jake@protonmail.com>
2022-07-08 15:29:46 -0500

    Added temp page to test user db operations

--- src/main/java/com/giftexchange/gex3/Gex3Controller.java
+++ src/main/java/com/giftexchange/gex3/Gex3Controller.java
@@ -1,9 +1,15 @@
 package com.giftexchange.gex3;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.giftexchange.gex3.user.User;
+import com.giftexchange.gex3.user.UserRepository;
 import com.giftexchange.gex3.util.NavblockGenerator;
 
 @Controller
@@ -43,4 +49,17 @@ public class Gex3Controller {
         return "create";
     }
     
+    @Autowired
+    private UserRepository userRepository;
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    @GetMapping(path="add")
+    public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String password){
+
+        User user = new User(name, encoder.encode(password));
+        userRepository.save(user);
+        return "Saved";
+    }
+
 }

e746ce5addbf6b57e0ce664c1cc3df425e8f35d2
Jake <herrmann.jake@protonmail.com>
2022-07-14 12:39:11 -0500

    Account forms use the same stompClient connection

--- src/main/resources/static/js/WebsocketForm.js
+++ src/main/resources/static/js/WebsocketForm.js
@@ -1,7 +1,5 @@
 export default class WebsocketForm {
 
-    stompClient = null;
-
     /**
     * @param {string} listenerURL The URL which the STOMP client subscribes to and recieves messages from.
     * @param {string} senderURL The URL which the STOMP client sends information to. This should be the URL the Controller is listening to.
@@ -9,25 +7,18 @@ export default class WebsocketForm {
     * @param {string} outputTextID The id of the element which should recieve data sent by the server.
     * @param {string} errorOutputTextID The id of the element which should recieve data sent by the server.
     * @param {list} fieldIDs The ids of all the fields in the form.
-    * @param {string} socketName The name of the websocket prefix, defined in `WebSocketConfig.java`. Defaults to 'websocket'.
     */
-    constructor(listenerURL, senderURL, formID, outputTextID, errorOutputTextID, fieldIDs, socketName = "/websocket") {
+    constructor(stompClient, listenerURL, senderURL, formID, outputTextID, errorOutputTextID, fieldIDs) {
+        this.stompClient = stompClient;
         this.listenerURL = listenerURL;//'/socket/account'
         this.senderURL = senderURL;//'app/accountSocket'
         this.formID = formID;
         this.outputTextID = outputTextID;
         this.errorOutputTextID = errorOutputTextID;
         this.fieldIDs = fieldIDs;
-        this.socketName = socketName;
-    }
 
-    connect() {
-        var socket = new SockJS(this.socketName);
-        this.stompClient = Stomp.over(socket);
-        this.stompClient.connect({},  () => {
-            this.stompClient.subscribe(this.listenerURL, (response) => {
-                this.outputData(response.body);
-            });
+        this.stompClient.subscribe(this.listenerURL, (response) => {
+            this.outputData(response.body);
         });
     }
 
--- src/main/resources/static/js/account.js
+++ src/main/resources/static/js/account.js
@@ -1,13 +1,18 @@
 import WebsocketForm from "./WebsocketForm.js";
 
-const InterestForm = new WebsocketForm("/socket/account/interest", "/app/account/interest", "interestForm", "#interest-text", "#interest-error-text-div", ["#interest-box"]);
-//const BirthdayForm = new WebsocketForm("/socket/account/interest", "/app/account/interest", "interestForm", "#interest-text", false, ["#interest-box"]);
-//const EmailForm = new WebsocketForm("/socket/account/interest", "/app/account/interest", "interestForm", "#interest-text", false, ["#interest-box"]);
-const PasswordForm = new WebsocketForm("/socket/account/password", "/app/account/password", "passwordForm", "#passwordForm", "#passwordForm", ["#current-box", "#new-box", "#confirm-box"]);
-
     $(function () {
-        InterestForm.connect();
-        PasswordForm.connect();
+        var socket = new SockJS("/websocket");
+        var stompClient = Stomp.over(socket);
+        
+        var InterestForm;
+        var PasswordForm;
+
+        stompClient.connect({}, ()=> {
+            InterestForm = new WebsocketForm(stompClient, "/socket/account/interest", "/app/account/interest", "interestForm", "#interest-text", "#interest-error-text-div", ["#interest-box"]);
+            //const BirthdayForm = new WebsocketForm("/socket/account/interest", "/app/account/interest", "interestForm", "#interest-text", false, ["#interest-box"]);
+            //const EmailForm = new WebsocketForm("/socket/account/interest", "/app/account/interest", "interestForm", "#interest-text", false, ["#interest-box"]);
+            PasswordForm = new WebsocketForm(stompClient, "/socket/account/password", "/app/account/password", "passwordForm", "#passwordForm", "#password-error-div", ["#current-box", "#new-box", "#confirm-box"]);
+        });
 
         $("#interestForm").on('submit', function (e) {
             e.preventDefault();

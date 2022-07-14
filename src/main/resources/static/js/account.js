b545e4eab41d518a1cf0ab83c4b80209ea87025c
Jake <herrmann.jake@protonmail.com>
2022-07-14 13:04:38 -0500

    Updated password to output errors to #passwordForm

--- src/main/resources/static/js/account.js
+++ src/main/resources/static/js/account.js
@@ -11,7 +11,7 @@ import WebsocketForm from "./WebsocketForm.js";
             InterestForm = new WebsocketForm(stompClient, "/socket/account/interest", "/app/account/interest", "interestForm", "#interest-text", "#interest-error-text-div", ["#interest-box"]);
             //const BirthdayForm = new WebsocketForm("/socket/account/interest", "/app/account/interest", "interestForm", "#interest-text", false, ["#interest-box"]);
             //const EmailForm = new WebsocketForm("/socket/account/interest", "/app/account/interest", "interestForm", "#interest-text", false, ["#interest-box"]);
-            PasswordForm = new WebsocketForm(stompClient, "/socket/account/password", "/app/account/password", "passwordForm", "#passwordForm", "#password-error-div", ["#current-box", "#new-box", "#confirm-box"]);
+            PasswordForm = new WebsocketForm(stompClient, "/socket/account/password", "/app/account/password", "passwordForm", "#passwordForm", "#passwordForm", ["#current-box", "#new-box", "#confirm-box"]);
         });
 
         $("#interestForm").on('submit', function (e) {

2c27c468d1eeb2fdc14de35e14fb895ea5e74a75
Jake <herrmann.jake@protonmail.com>
2022-07-21 07:52:13 -0500

    Adds method to select items for specific owner

--- src/main/java/com/giftexchange/gex3/item/ItemController.java
+++ src/main/java/com/giftexchange/gex3/item/ItemController.java
@@ -1,5 +1,7 @@
 package com.giftexchange.gex3.item;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +12,13 @@ import com.giftexchange.gex3.util.NavblockGenerator;
 @Controller
 public class ItemController {
 
+    @Autowired
+    private ItemRepository itemRepository;
+
     @GetMapping("/")
-    public String manage(Model model){
+    public String manage(Model model, Authentication auth){
         model.addAttribute("navblock", NavblockGenerator.generateNavblock(Constants.NAVBLOCK_MAP, "Your List"));
+        model.addAttribute("items", itemRepository.findAllWithOwner(auth.getName()));
         return "manage";
     }
 
--- src/main/java/com/giftexchange/gex3/item/ItemRepository.java
+++ src/main/java/com/giftexchange/gex3/item/ItemRepository.java
@@ -1,9 +1,13 @@
 package com.giftexchange.gex3.item;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ItemRepository extends CrudRepository<ItemTable, Integer> {
 
     ItemTable findByName(String name);
+
+    @Query(value = "SELECT * FROM item WHERE owner = ?", nativeQuery = true)
+    Iterable<ItemTable> findAllWithOwner(String owner);
 
 }

25523d6fc060cfb8f681ca21141b6b1bdfe5929c
Jake <herrmann.jake@protonmail.com>
2022-07-21 07:52:38 -0500

    Removed bought constant, added html constants

--- src/main/java/com/giftexchange/gex3/gex/Constants.java
+++ src/main/java/com/giftexchange/gex3/gex/Constants.java
@@ -15,6 +15,11 @@ public final class Constants {
     public static final String CSS_DISMISSABLE_SUCCESS_MODAL = "<div class='alert alert-success alert-dismisible mt-3'><button type='button' class='close' data-dismiss='alert'>&times;</button>";
     public static final String CSS_DISMISSABLE_ERROR_MODAL = "<div class='alert alert-danger alert-dismissible mt-3'><button type='button' class='close' data-dismiss='alert'>&times;</button>";
 
+    //HTML Snippets
+    public static final String HTML_SELF_ITEM_CARD = "<div class='card'><div class='card-header'><h4 class='card-title'>{name}</h4></div><div class='card-body'><a class='card-link' target='_blank' href='{url}'>{title}</a><p class='card-text'>{comment}</p><p class='card-text'>{entryDate}<span class='text-danger'>*</span></p></div</div>";
+    public static final String HTML_UNBOUGHT_ITEM_CARD = "<div class='card'><div class='card-header'><h4 class='card-title'>{name}</h4></div><div class='card-body'><a class='card-link' target='_blank' href='{url}'>{title}</a><p class='card-text'>{comment}</p><p class='card-text'>{entryDate}<span class='text-danger'>*</span></p><p class='card-text'>This item has not been bought</p><button class='btn btn-primary'>Mark Item As Bought</button></div></div>";
+    public static final String HTML_BOUGHT_ITEM_CARD = "<div class='card'><div class='card-header'><h4 class='card-title'>{name}</h4></div><div class='card-body'><a class='card-link' target='_blank' href='{url}'>{title}</a><p class='card-text'>{comment}</p><p class='card-text'>{entryDate}<span class='text-danger'>*</span></p><p class='card-text text-danger'>This item has been bought by {boughtBy}</p></div></div>";
+
     public static void initNavblockConstant(){
         NAVBLOCK_MAP.add(new HashMap<String, String>());
         NAVBLOCK_MAP.get(0).put("name", "Account");
@@ -29,10 +34,6 @@ public final class Constants {
         NAVBLOCK_MAP.get(2).put("url", "/");
 
         NAVBLOCK_MAP.add(new HashMap<String, String>());
-        NAVBLOCK_MAP.get(3).put("name", "Bought");
-        NAVBLOCK_MAP.get(3).put("url", "/bought");
-
-        NAVBLOCK_MAP.add(new HashMap<String, String>());
-        NAVBLOCK_MAP.get(4).put("name", "Logout");
+        NAVBLOCK_MAP.get(3).put("name", "Logout");
         }
     }

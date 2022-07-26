aa5baf1af670ed5c0ce68ac4a33f004c496dbedf
Jake <herrmann.jake@protonmail.com>
2022-07-26 09:47:03 -0500

    Added list.html and controller binding

--- src/main/java/com/giftexchange/gex3/item/ItemController.java
+++ src/main/java/com/giftexchange/gex3/item/ItemController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.giftexchange.gex3.gex.Constants;
 import com.giftexchange.gex3.util.NavblockGenerator;
@@ -21,5 +22,17 @@ public class ItemController {
         model.addAttribute("items", itemRepository.findAllWithOwner(auth.getName()));
         return "manage";
     }
+
+    @GetMapping("/users/{owner}")
+    public String userItems(@PathVariable("owner") String owner, Model model, Authentication auth){
+        if(owner.equals(auth.getName())){
+            return "redirect:/";
+        }
+        model.addAttribute("navblock", NavblockGenerator.generateNavblock(Constants.NAVBLOCK_MAP, ""));
+        model.addAttribute("owner", owner);
+        model.addAttribute("items", itemRepository.findAllWithOwner(owner));
+        return "list";
+    }
+
 
 }

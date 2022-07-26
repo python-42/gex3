a66d77a69b43fc49de916a5c554366fc522bbf5e
Jake <herrmann.jake@protonmail.com>
2022-07-25 20:09:32 -0500

    Updated to use JPQL and added new query

--- src/main/java/com/giftexchange/gex3/item/ItemRepository.java
+++ src/main/java/com/giftexchange/gex3/item/ItemRepository.java
@@ -2,12 +2,16 @@ package com.giftexchange.gex3.item;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ItemRepository extends CrudRepository<ItemTable, Integer> {
 
     ItemTable findByName(String name);
 
-    @Query(value = "SELECT * FROM item WHERE owner = ?", nativeQuery = true)
-    Iterable<ItemTable> findAllWithOwner(String owner);
+    @Query(value = "SELECT i FROM item i WHERE i.owner = :owner")
+    Iterable<ItemTable> findAllWithOwner(@Param("owner") String owner);
+
+    @Query(value = "SELECT DISTINCT i.owner FROM item i WHERE NOT i.owner = :owner")
+    Iterable<ItemTable> findAllDistinctOwnersExcept(@Param("owner") String owner);
 
 }

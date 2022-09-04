c3df44eeb2072c82d8ee602efea92486225d8d47
Jake <herrmann.jake@protonmail.com>
2022-09-04 17:52:43 -0500

    Removed unnecessary casting

--- src/main/java/com/giftexchange/gex3/item/ItemService.java
+++ src/main/java/com/giftexchange/gex3/item/ItemService.java
@@ -13,11 +13,11 @@ public class ItemService {
     public static String itemForm(ItemRepository itemRepository, WebsocketFormData data, String name){
         HashMap<String, String> dataMap = new HashMap<String, String>();
 
-        String id = (String) data.getDataPart(0);
-        dataMap.put("name", (String)data.getDataPart(1));
-        dataMap.put("url", (String)data.getDataPart(2));
-        dataMap.put("title", (String)data.getDataPart(3));
-        dataMap.put("comment", (String)data.getDataPart(4));
+        String id = data.getDataPart(0);
+        dataMap.put("name", data.getDataPart(1));
+        dataMap.put("url", data.getDataPart(2));
+        dataMap.put("title", data.getDataPart(3));
+        dataMap.put("comment", data.getDataPart(4));
 
         for (Entry<String, String> x : dataMap.entrySet()){
             if(x.getValue() == null || x.getValue() == ""){
@@ -33,7 +33,7 @@ public class ItemService {
         if(id.equals("")){
             itemRepository.save(new ItemTable(name, dataMap.get("name"), dataMap.get("url"), dataMap.get("title"), dataMap.get("comment")));
         }else{
-            if(itemRepository.itemOwnerById(Integer.valueOf(id)) == name){
+            if(itemRepository.itemOwnerById(Integer.valueOf(id)).equals(name)){
                 itemRepository.updateSelfItemForID(dataMap.get("name"), dataMap.get("url"), dataMap.get("title"), dataMap.get("comment"), Integer.valueOf(id));
             }else{
                 return "An error occured: Item ID and owner mismatch. Try reloading the page.";

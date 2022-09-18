41155b3d471e049feb497d7b63bea45ca204c083
Jake <herrmann.jake@protonmail.com>
2022-09-18 18:12:17 -0500

    Created Elements enum to store id of templates

---
+++ src/main/java/com/giftexchange/gex3/item/Elements.java
@@ -0,0 +1,10 @@
+package com.giftexchange.gex3.item;
+
+public enum Elements {
+    ROW("#row"),
+    ERROR_MODAL("#errorModal");
+
+    public final String value;
+
+    Elements(String value){this.value = value;}
+}

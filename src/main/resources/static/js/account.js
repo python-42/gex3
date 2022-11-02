b192bce7c6afaede8273c67f6a0691aa9afe1280
Jake <herrmann.jake@protonmail.com>
2022-11-01 20:05:53 -0500

    Updated account.js to use vanilla javascript

--- src/main/resources/static/js/account.js
+++ src/main/resources/static/js/account.js
@@ -1,26 +1,27 @@
 import WebsocketForm from "./WebsocketForm.js";
 
-    $(function () {
-        var socket = new SockJS("/websocket");
-        var stompClient = Stomp.over(socket);
-        
-        var InterestForm;
-        var PasswordForm;
+document.addEventListener("DOMContentLoaded", function(event) { 
+    var socket = new SockJS("/websocket");
+    var stompClient = Stomp.over(socket);
+    
+    var InterestForm;
+    var PasswordForm;
 
-        stompClient.connect({}, ()=> {
-            InterestForm = new WebsocketForm(stompClient, "/socket/account/interest", "/app/account/interest", "interestForm", "#interest-text", "#interest-error-text-div", ["#interest-box"]);
-            //const BirthdayForm = new WebsocketForm("/socket/account/interest", "/app/account/interest", "interestForm", "#interest-text", false, ["#interest-box"]);
-            //const EmailForm = new WebsocketForm("/socket/account/interest", "/app/account/interest", "interestForm", "#interest-text", false, ["#interest-box"]);
-            PasswordForm = new WebsocketForm(stompClient, "/socket/account/password", "/app/account/password", "passwordForm", "#passwordForm", "#passwordForm", ["#current-box", "#new-box", "#confirm-box"]);
-        });
-
-        $("#interestForm").on('submit', function (e) {
-            e.preventDefault();
-            InterestForm.sendData();
-        });
-
-        $("#passwordForm").on('submit', function (e) {
-            e.preventDefault();
-            PasswordForm.sendData();
-        });
+    stompClient.connect({}, ()=> {
+        InterestForm = new WebsocketForm(stompClient, "/socket/account/interest", "/app/account/interest", "interestForm", "interest-text", "interest-error-text-div", ["interest-box"]);
+        //const BirthdayForm = new WebsocketForm("/socket/account/interest", "/app/account/interest", "interestForm", "interest-text", false, ["interest-box"]);
+        //const EmailForm = new WebsocketForm("/socket/account/interest", "/app/account/interest", "interestForm", "interest-text", false, ["interest-box"]);
+        PasswordForm = new WebsocketForm(stompClient, "/socket/account/password", "/app/account/password", "passwordForm", "passwordForm", "passwordForm", ["current-box", "new-box", "confirm-box"]);
     });
+
+    document.getElementById("interestForm").addEventListener("submit", function(e) {
+        e.preventDefault();
+        InterestForm.sendData();
+    });
+
+    document.getElementById("passwordForm").addEventListener("submit", function(e) {
+        e.preventDefault();
+        PasswordForm.sendData();
+    });
+
+});
